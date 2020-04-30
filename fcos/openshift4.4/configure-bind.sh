b3058e164a8647c858cce0ca6c20729ec9aa424a
zerodayz <cerninr@gmail.com>
2020-04-30 23:18:14 +1000

    fix: mkdir zones

--- fcos/openshift4.4/configure-bind.sh
+++ fcos/openshift4.4/configure-bind.sh
@@ -4,6 +4,7 @@ sudo yum -y install bind
 pushd named-configuration
 sudo cp named.conf /etc/named.conf
 sudo cp named.conf.local /etc/named/named.conf.local
+sudo mkdir /var/named/zones/
 sudo cp db.192.168.122 /var/named/zones/db.192.168.122
 sudo cp db.fcos.k8s.local /var/named/zones/db.fcos.k8s.local
 
